773883879b4d4fc10ad7a88534c134ce171c3bd5
LaliGulbani <lalushka@gmail.com>
2019-02-18 22:22:14 +0300

    HW-8 Created interceptor to pass Authorization token received from BE for each API call

--- angular-course/src/app/app-routing.module.ts
+++ angular-course/src/app/app-routing.module.ts
@@ -1,14 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { CoursesModule } from './courses/courses.module';
+import { LoginModule } from './login/login.module';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routes';
-import {AuthGuard} from './core/guards/auth.guard';
-import {AlreadyLoggedInGuard} from './core/guards/already-logged-in.guard';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {TokenInterceptorService} from './core/services/token-interceptor.service';
 
 @NgModule({
+  declarations: [
+    AppComponent
+  ],
   imports: [
+    BrowserModule,
+    HttpClientModule,
+    CoreModule,
+    CoursesModule,
+    LoginModule,
     RouterModule.forRoot(ROUTES)
   ],
-  exports: [RouterModule],
-  providers: [AuthGuard, AlreadyLoggedInGuard]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    }],
+  bootstrap: [AppComponent]
 })
-export class AppRoutingModule { }
+export class AppModule { }
