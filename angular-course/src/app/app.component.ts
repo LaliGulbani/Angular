f870e2fb751c21e3ab3bd6e50038984a46dbb484
LaliGulbani <lalushka@gmail.com>
2019-02-18 23:39:49 +0300

    HW-7 Implement breadcrumbs

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  public title = 'angular-course';
+export class AppComponent implements OnInit {
+  public pageTitle = ''
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.pageTitle = this.route.snapshot.data['title'];
+  }
 }
